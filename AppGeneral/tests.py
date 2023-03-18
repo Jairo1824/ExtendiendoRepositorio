2a4389861a5e0d3f02c0c80ebba5f1b992b00f39
Jairo1824 <jairo_esteban_93@live.com>
2023-03-18 12:10:02 -0300

    creacion de pruebas de test

--- AppGeneral/tests.py
+++ AppGeneral/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from AppGeneral.models import Persona
 
 # Create your tests here.
+class URLTest(TestCase):
+    def test_inicio(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code,200)
+
+
+class TestModels(TestCase):
+    def setUp(self) :
+        self.persona=Persona.objects.create(
+            nombre="soy un nombre",
+            apellido="soy una persona",
+            fecha_nacimiento="1933-02-09",
+            email="soyemail@email.com",
+        )
+
+    def test_modulo(self):
+        self.assertEqual(self.persona.nombre,"soy un nombre")
+
+    def setUp(self) :
+        self.persona=Persona.objects.create(
+            nombre="soy un nombre",
+            apellido="soy una persona",
+            fecha_nacimiento="1933-02-09",
+            email="soyemailemail.com",
+        )
+
+    def test_modulo(self):
+        self.assertFalse((self.persona.email))
